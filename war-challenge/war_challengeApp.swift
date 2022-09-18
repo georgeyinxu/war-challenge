047eb559b99096acba4a0a5250714f851d3a4bc5
George Yin Xu <georgeyinxu@gmail.com>
2022-09-18 19:56:28 +0800

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by George Yin Xu on 18/9/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
